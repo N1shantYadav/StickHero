fc2e24cd49112948a1eb267ff829590288276ec4
N1shantYadav <113256067+N1shantYadav@users.noreply.github.com>
2023-11-16 11:26:30 +0530

    Add files via upload

---
+++ GameFeatures.java
@@ -0,0 +1,10 @@
+package com.example.stickhero;
+
+public interface GameFeatures {
+
+    public static void saveGame(int score, int cherries){
+
+    }
+    public void restartGame();
+}
+
---
+++ GameManager.java
@@ -0,0 +1,6 @@
+package com.example.stickhero;
+
+public class GameManager {
+
+
+}
